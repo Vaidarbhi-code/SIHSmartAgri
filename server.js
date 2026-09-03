5aa209d5863bbe58e0eaa397bcbc6d18c38647a8
Vaidarbhi-code <vaidarbhibhandakkar@gmail.com>
2026-09-04 00:08:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const path = require("path");
 
 const app = express();
 
+// Serve all frontend files
 app.use(express.static(__dirname));
 
-app.get("*", (req, res) => {
+// Send index.html for the main page
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, "0.0.0.0", () => {
-    console.log(`SmartAgri running on port ${PORT}`);
+    console.log(`SmartAgri server running on port ${PORT}`);
 });
